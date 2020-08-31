5ebbc53c6086161a85e6346adf260c8cf34cbb4b
gyy <gouyy@fiscan.cn>
2020-08-31 15:57:22 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace pjsua2_csharp_video_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInit = new System.Windows.Forms.Button();
+            this.btnSelectCamra = new System.Windows.Forms.Button();
+            this.comboxCamre = new System.Windows.Forms.ComboBox();
+            this.panelCamera = new System.Windows.Forms.Panel();
+            this.btnStartCamera = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(53, 74);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(113, 35);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "初始化";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // btnSelectCamra
+            // 
+            this.btnSelectCamra.Location = new System.Drawing.Point(53, 137);
+            this.btnSelectCamra.Name = "btnSelectCamra";
+            this.btnSelectCamra.Size = new System.Drawing.Size(113, 35);
+            this.btnSelectCamra.TabIndex = 1;
+            this.btnSelectCamra.Text = "获取摄像头";
+            this.btnSelectCamra.UseVisualStyleBackColor = true;
+            this.btnSelectCamra.Click += new System.EventHandler(this.btnSelectCamra_Click);
+            // 
+            // comboxCamre
+            // 
+            this.comboxCamre.DropDownHeight = 130;
+            this.comboxCamre.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboxCamre.FormattingEnabled = true;
+            this.comboxCamre.IntegralHeight = false;
+            this.comboxCamre.Location = new System.Drawing.Point(194, 142);
+            this.comboxCamre.Name = "comboxCamre";
+            this.comboxCamre.Size = new System.Drawing.Size(247, 29);
+            this.comboxCamre.TabIndex = 2;
+            // 
+            // panelCamera
+            // 
+            this.panelCamera.Location = new System.Drawing.Point(53, 286);
+            this.panelCamera.Name = "panelCamera";
+            this.panelCamera.Size = new System.Drawing.Size(735, 432);
+            this.panelCamera.TabIndex = 3;
+            // 
+            // btnStartCamera
+            // 
+            this.btnStartCamera.Location = new System.Drawing.Point(53, 219);
+            this.btnStartCamera.Name = "btnStartCamera";
+            this.btnStartCamera.Size = new System.Drawing.Size(113, 35);
+            this.btnStartCamera.TabIndex = 4;
+            this.btnStartCamera.Text = "启动摄像头";
+            this.btnStartCamera.UseVisualStyleBackColor = true;
+            this.btnStartCamera.Click += new System.EventHandler(this.btnStartCamera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 730);
+            this.Controls.Add(this.btnStartCamera);
+            this.Controls.Add(this.panelCamera);
+            this.Controls.Add(this.comboxCamre);
+            this.Controls.Add(this.btnSelectCamra);
+            this.Controls.Add(this.btnInit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Button btnSelectCamra;
+        private System.Windows.Forms.ComboBox comboxCamre;
+        private System.Windows.Forms.Panel panelCamera;
+        private System.Windows.Forms.Button btnStartCamera;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace pjsua2_csharp_video_demo
+{
+    public partial class Form1 : Form
+    {
+        public static Endpoint ep;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            ep = new Endpoint();
+            ep.libCreate();
+
+            EpConfig epConfig = new EpConfig();
+            epConfig.logConfig.writer = new DebugLog();
+
+            ep.libInit(epConfig);
+
+            TransportConfig tcfg = new TransportConfig();
+            tcfg.port = 5060;
+
+            ep.transportCreate(pjsip_transport_type_e.PJSIP_TRANSPORT_UDP,tcfg);
+            ep.transportCreate(pjsip_transport_type_e.PJSIP_TRANSPORT_TCP,tcfg);
+            // Start library
+            ep.libStart();
+            this.btnInit.Text = "初始化成功";
+            this.btnInit.Enabled = false;
+        }
+        /// <summary>
+        /// 获取摄像头
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectCamra_Click(object sender, EventArgs e)
+        {
+            var videoList = ep.vidDevManager().enumDev2();
+            foreach(var item in videoList)
+            {
+                ListItems listItems = new ListItems(item.id.ToString(), item.name);
+                comboxCamre.Items.Add(listItems);
+            }
+        }
+
+        private void btnStartCamera_Click(object sender, EventArgs e)
+        {
+            
+            var selectCamera = (ListItems)comboxCamre.SelectedItem;
+            //VideoPreview preview = new VideoPreview(int.Parse(selectCamera.Key));
+            //VideoPreviewOpParam videoPreviewOpParam = new VideoPreviewOpParam();
+            //videoPreviewOpParam.window = new VideoWindowHandle();
+            //videoPreviewOpParam.show = true;
+            //videoPreviewOpParam.window.handle = new WindowHandle();
+            //videoPreviewOpParam.window.handle.setWindow(this.panelCamera.Handle.ToInt64());
+
+
+            //preview.start(videoPreviewOpParam);
+
+            VideoPreview video = new VideoPreview((int)pjmedia_vid_dev_std_index.PJMEDIA_VID_DEFAULT_CAPTURE_DEV);            
+            VideoPreviewOpParam videoPreviewOpParam = new VideoPreviewOpParam();
+            videoPreviewOpParam.rendId = (int)pjmedia_vid_dev_std_index.PJMEDIA_VID_DEFAULT_RENDER_DEV;
+            videoPreviewOpParam.show = true;
+
+            video.start(videoPreviewOpParam);
+            var window = video.getVideoWindow();
+            var videoList = ep.vidDevManager().enumDev2();
+      
+            //var epdev = ep.vidDevManager().getOutputWindowFlags(-1);
+            window.Show(false);
+
+            MediaCoordinate coordinate = new MediaCoordinate();
+            coordinate.x = 0;// this.panelCamera.Top;
+            coordinate.y = 0;// this.panelCamera.Left;
+            window.setPos(coordinate);
+
+            MediaSize size = new MediaSize();
+            size.h = (uint)this.panelCamera.Height;
+            size.w = (uint)this.panelCamera.Width;
+            window.setSize(size);
+
+            var intPtr = EmbedControl.FindWindow(null, "pjmedia-SDL video");
+            EmbedControl.SetParent(intPtr, this.panelCamera.Handle);
+            //window.getInfo().isNative = false;
+            window.Show(true);
+
+            window.Show(true);
+            //window.setFullScreen(true);
+            //window.rotate(90);
+
+            //VideoWindowHandle windowHandle = new VideoWindowHandle();
+            //windowHandle.handle = new WindowHandle();
+            //windowHandle.handle.setWindow(this.panelCamera.Handle.ToInt64());
+
+            //window.setWindow(windowHandle);
+            //window.rotate(90);
+
+            //windowInfo.winHandle.handle.setWindow(this.panelCamera.Handle.ToInt64());
+            //window.Show(true);
+
+
+            //var windowInfo = window.getInfo();
+            //windowInfo.isNative = false;
+            //windowInfo.      
+        }
+    }
+}
